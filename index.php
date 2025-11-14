b75d51e34de8f54885e88c73a8e3ea2b0e97f536
Megumi Iwasa <peachpee316@gmail.com>
2025-11-14 22:05:33 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -50,10 +50,10 @@
      <!-- Clarityヒートマップ -->
      <script type="text/javascript">
      (function(c,l,a,r,i,t,y){
-     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-     })(window, document, "clarity", "script", "u36tlcfezn");
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+     })(window, document, "clarity", "script", "u5zepaeqy2");
      </script>
 
 </head>
